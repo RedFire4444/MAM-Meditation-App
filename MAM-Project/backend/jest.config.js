16ca9e593e9920660d9a27ca2719ed63ef824c19
L A Z ☰ R - W A V ☰ <krupal.warale231@vit.edu>
2026-04-01 16:14:31 +0530

    Add Jest configuration file for testing setup

---
+++ MAM-Project/backend/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: [
+    '**/tests/**/*.test.js'
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{js,ts}',
+    '!src/**/*.d.ts',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
+  testTimeout: 30000, // 30 seconds for database operations
+  verbose: true
+};
